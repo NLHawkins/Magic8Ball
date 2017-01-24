0f5c8ac743cd78141a2d9f5beedf64d9891701c2
NLHawkins <nathanlhawkins@gmail.com>
2017-01-24 08:57:01 -0500

    final 8ball

---
+++ EightBallTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EightBallTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RandomEightBallisChosen()
+        {
+
+        }
+    }
+}
